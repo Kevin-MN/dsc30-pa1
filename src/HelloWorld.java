4621ce1d7fabd4b31083b1133cbe7450eb7acdef
Kevin Morales-Nguyen <Kevin.t.moralesnguyen@gmail.com>
2021-10-02 21:33:01 -0700

    Completed all method implementations, completed java docs

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,16 @@
+/**
+ * Name: Kevin Morales-Nguyen
+ * PID: A17186624
+ */
+
+
+/**
+ * This class is used alongside the setup and usage of github
+ * which is required for PA 1.
+ *
+ * @author Kevin Morales-Nguyen
+ * @since 9/24/2021
+ */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
--- src/Startup.java
+++ src/Startup.java
@@ -8,6 +8,9 @@ import java.io.FileNotFoundException;
 import java.util.*;
 
 /**
+ * This class is used to implement and test methods defined in
+ * programming assignment 1
+ *
  * @author Kevin Morales-Nguyen
  * @since 9/24/2021
  */
@@ -17,6 +20,16 @@ public class Startup {
     private static int NUM_FILES = 3;
     private static int ASCII_ZERO = 48;
 
+    /**
+     *This method will print out a list of related measurements, either weight or
+     * distance, that are converted between different units.
+     *
+     * @param option 1 specifies weights conversions and 2 specifies distance conversions
+     * @param currentUnit the starting unit type that is to be converted to other types,
+     *                    dependent on option 1
+     * @return String that has additional conversions for weight/distance based on the
+     * option argument
+     */
     public static String convertUnits(int option, String currentUnit, double value) {
         double kg, gram, pound, ounce; // variables for weights
         kg = gram = pound = ounce = 0.00;
@@ -54,54 +67,63 @@ public class Startup {
                     return "Invalid unit";
                 }
 
-                return "kg: " + String.format("%.2f", kg) + "\ngram: " + String.format("%.2f", gram) + "\npounds: " + String.format("%.2f", pound) + "\nounce: " + String.format("%.2f", ounce);
+                return "kg: " + String.format("%.2f", kg) + "\ngram: " +
+                        String.format("%.2f", gram) + "\npounds: " +
+                        String.format("%.2f", pound) + "\nounce: " +
+                        String.format("%.2f", ounce);
             case 2:
-                if (currentUnit == "meter") {
+                if (currentUnit.equals("meter")) {
                     meter = value;
 
                     mile = meter * 0.000621371192;
                     yard = meter * 1.09361329834;
                     foot = meter * 3.280839895013;
-                } else if (currentUnit == "mile") {
+                } else if (currentUnit.equals("mile")) {
                     mile = value;
 
                     meter = mile * 1609.344;
                     yard = mile * 1760;
                     foot = mile * 5280;
 
-
-                } else if (currentUnit == "yard") {
+                } else if (currentUnit.equals("yard")) {
                     yard = value;
 
                     meter = yard * 0.9144;
                     mile = yard * 0.000568181818;
                     foot = yard * 3;
-                } else if (currentUnit == "foot") {
+
+                } else if (currentUnit.equals("foot")) {
                     foot = value;
 
                     meter = foot * 0.3048;
                     mile = foot * 0.000189393939;
                     yard = foot * 0.333333333333;
+
                 } else {
                     return "Invalid unit";
                 }
 
-                return "meter: " + String.format("%.2f", meter) + "\nmile: " + String.format("%.2f", mile) + "\nyard: " + String.format("%.2f", yard) + "\nfoot: " + String.format("%.2f", foot);
-
+                return "meter: " + String.format("%.2f", meter) + "\nmile: " +
+                        String.format("%.2f", mile) + "\nyard: " +
+                        String.format("%.2f", yard) + "\nfoot: " +
+                        String.format("%.2f", foot);
             default:
                 return "Invalid option.\n";
         }
     }
 
-
+    /**
+     *This method takes in a string along with a delimiter and returns a string that has
+     * segments removed based on the position of the pair(s) of delimiters
+     *
+     * @param input raw string of text that includes delimiters in paris for removing text
+     * @param boundary the character that specifies the start or end of text to be removed
+     * @return A string that has removed text specified by the boundary argument
+     */
     public static String removeComments(String input, char boundary) {
-
         if (input.contains(String.valueOf(boundary))) {
             ArrayList<Integer> indeces = new ArrayList<Integer>();
-            char[] input_chars = new char[100000];
-
-            input.getChars(0, input.length(), input_chars, 0);
-
+            char[] input_chars = input.toCharArray();
 
             for (int i = 0; i < input.length(); i++) {
                 if (input_chars[i] == boundary) {
@@ -112,15 +134,10 @@ public class Startup {
             String result = "";
             boolean add;
 
-
             if (indeces.get(0) == 0) {
                 add = false;
             } else {
                 add = true;
-            }
-
-            for (int l = 0; l < indeces.size(); l++) {
-                System.out.println(indeces.get(l));
             }
 
             for (int l = 0; l < input_chars.length; l++) {
@@ -134,57 +151,50 @@ public class Startup {
                     add = true;
                 }
             }
-            result = result.replace(String.valueOf('\u0000'), "");
 
             return result;
 
-        } else {
+        }
+        else {
             return input;
         }
 
     }
 
+    /**
+     *This method uses the Random class to generate random integers and then compares
+     * them to determine who should get the seat, the person with the largest sum of digits,
+     * or lower number of overall digits if the sum is equal, wins
+     *
+     * @return A string that describes the winner and the value they won with
+     */
     public static String determineSeat() {
-        Random random = new Random();
+        Random random = new Random(1);
 
         int int1, int2, sum1, sum2;
         int1 = int2 = sum1 = sum2 = 0;
         String int_str1, int_str2;
         int_str1 = int_str2 = "";
-
-
         boolean battle = true;
         boolean winner = false;
 
         while (battle) {
-            int1 = random.nextInt(13);
-            int2 = random.nextInt(4);
+            int1 = random.nextInt(100);
+            int2 = random.nextInt(100);
 
             int_str1 = String.valueOf(int1);
             int_str2 = String.valueOf(int2);
-
-            System.out.println("Random int 1: "  + int_str1);
-            System.out.println("Random int 2: " + int_str2);
-
 
             char[] int1_chars = int_str1.toCharArray();
             char[] int2_chars = int_str2.toCharArray();
 
             for (int i = 0; i < int1_chars.length; i++) {
                 sum1 += Integer.parseInt(String.valueOf(int1_chars[i]));
-                System.out.println(int1_chars[i]);
             }
 
             for (int l = 0; l < int2_chars.length; l++) {
                 sum2 += Integer.parseInt(String.valueOf(int2_chars[l]));
-                System.out.println(int2_chars[l]);
             }
-
-            System.out.println("Sum 1: " + sum1);
-            System.out.println("Sum 2: " + sum2);
-
-            //System.out.println(int1 + " -- " + int_str1 + " -- " +  int1_chars.length + " -- " +  int_str1.length());
-            //System.out.println(int2 + " -- " + int_str2 + " -- " +  int2_chars.length + " -- " +  int_str2.length());
 
             if (sum1 > sum2) {
                 winner = true;
@@ -214,61 +224,85 @@ public class Startup {
         }
     }
 
+    /**
+     *This method takes a total amount of money and processes how much can be transferred
+     * after transaction fees have been subtracted, it also will not transfer some funds if
+     * the appropriate fee requirements are not met.
+     *
+     * @param amount the total amount of money input
+     * @return A string that describes the amount available to transfer and the
+     * fees accrued to initiate the transfer
+     */
     public static String processTransfer(int amount){
 
         int max_transfer = 0;
         int fees = 0;
 
         while(amount > 7){
-
             if(amount > 107){
                 max_transfer += 100;
                 fees += 7;
                 amount -= 107;
-            }else{
+            }
+            else{
                 max_transfer += (amount - 7);
                 fees+= 7;
                 amount -= (amount + 7);
             }
-
         }
-
         return "Max amount: " + max_transfer + ", transaction fee is " +  fees ;
-
     }
 
+
+    /**
+     *This method takes a raw string which could have parentheses delimiters and
+     * returns a string that repeats the delimiters preceding character by N times,
+     * N being the numeric value within the parentheses
+     *
+     * @param input a raw string that could have parentheses delimiters
+     * @return A that has any delimiters removed and the appropriate amount of charaters
+     * printed out as specified by the delimiter
+     */
     public static String decompressString(String input){
 
         String result = "";
 
         if(input.contains(String.valueOf('('))){
             char[] chars = input.toCharArray();
-            ArrayList<Integer> indeces = new ArrayList<Integer>();
-
+            ArrayList<Integer> start_indeces = new ArrayList<Integer>();
+            ArrayList<Integer> end_indeces = new ArrayList<Integer>();
             for (int i = 0; i < input.length(); i++) {
                 if (chars[i] == '(') {
-                    indeces.add(i);
-                    System.out.println(i);
+                    start_indeces.add(i);
                 }
             }
 
+            for (int i = 0; i < input.length(); i++) {
+                if (chars[i] == ')') {
+                    end_indeces.add(i);
+                }
+            }
+
+
             for(int l = 0; l < chars.length;l++){
-                if(!indeces.isEmpty() && l == indeces.get(0)){
-                    for(int p = 1; p < Integer.parseInt(String.valueOf(chars[l+1])); p++){
-                        result = result.concat(String.valueOf(chars[l-1]));   //
-                        System.out.println("Entered inner mult loop");
+                if(!start_indeces.isEmpty() && l == start_indeces.get(0)){
+                    int num = 0;
+
+                    String number = input.substring(start_indeces.get(0) + 1,
+                            end_indeces.get(0));
+
+                    for(int p = 1; p < Integer.parseInt(String.valueOf(number)); p++){
+
+                        result = result.concat(String.valueOf(chars[l-1]));
                     }
 
-
-                    indeces.remove(0);
-                    l+=2;
+                    start_indeces.remove(0);
+                    end_indeces.remove(0);
+                    l+= number.length() + 1;
                 }
                 else{
                     result = result.concat(String.valueOf(chars[l]));
-
                 }
-
-
             }
 
             return result;
@@ -276,20 +310,17 @@ public class Startup {
         else{
             return input;
         }
-
-
-
-
-
-
-
-
-
-
-
     }
 
-
+    /**
+     *This method calculates a time table and prints it out as a string
+     * based on a given initial time and an array that contains subsequent times,
+     * the time is based on 24-hour format
+     *
+     * @param startTime A String that indicates the initail starting time for the time table
+     * @param intervals A integer array that contains the subsequent times for boarding
+     * @return
+     */
     public static String calculateTime(String startTime, int[] intervals){
 
         String[] time_split = startTime.split(":");
@@ -339,6 +370,20 @@ public class Startup {
         return startTime;
     }
 
+
+    /**
+     *This method prints a summary about the classes students based on the names and
+     * corresponding array that is used to keep track of symptoms for covid-19.
+     * The summary includes number of students who should stay home, their names, and the
+     * average number of symptoms per student.
+     *
+     *
+     * @param namesArray An String array that holds all the names of the students
+     * @param responses An 2d boolean Array that stores 0 indicated negative symptom or 1
+     *                  for a positive symptom
+     * @return A string that has detailed information about the number of students that should
+     * not attend class, the names of those students, and the average number of symptoms per student
+     */
     public static String summarizeResponse(String[] namesArray, boolean[][] responses){
 
         ArrayList<String> names = new ArrayList<String>();
@@ -362,13 +407,14 @@ public class Startup {
 
                 }
 
-            System.out.println(responses.length * responses[0].length);
-
             int num_sick = names.size();
                 String names_string = "The names are ";
 
+                if(num_sick == 0){
+                    names_string = names_string.concat("none");
 
-                if(num_sick == 1){
+                }
+                else if(num_sick == 1){
                     names_string = names_string.concat(names.get(0));
 
                 }
@@ -378,7 +424,7 @@ public class Startup {
                             names_string = names_string.concat(names.get(i) + ", ");
 
                         } else if (i == (names.size() - 1)) {
-                            names_string = names_string.concat(" and " + names.get(i));
+                            names_string = names_string.concat("and " + names.get(i));
                         } else {
                             names_string = names_string.concat(names.get(i) + ", ");
                         }
@@ -389,19 +435,21 @@ public class Startup {
                 double max_symptoms = responses.length * responses[0].length;
                 double total_symptoms_double = (double) total_symptoms;
                 double avg_symptoms =  total_symptoms_double /(double) namesArray.length ;
-                System.out.println(avg_symptoms);
 
-        return names.size() + "/" + namesArray.length + " students should stay home.\n" +  names_string + ".\n" +
-                "On this date, the students reported " + String.format("%.1f",avg_symptoms) + " symptoms on average.";
+
+        return names.size() + "/" + namesArray.length + " students should stay home.\n" +
+                names_string + ".\n" + "On this date, the students reported " +
+                String.format("%.1f",avg_symptoms) + " symptoms on average.";
 
     }
 
     public static void main(String[] args) {
 
+        //Below are the tests for #7. Input files are already transformed into a
+        // namesArray and a response arrayYour task would be to get information
+        // from the arrays and summarize the responses in desired format See input
+        // text and writeup for more details
 
-        //Below are the tests for #7. Input files are already transformed into a namesArray and a response array
-        //Your task would be to get information from the arrays and summarize the responses in desired format
-        //See input text and writeup for more details
         for(int i=1;i<NUM_FILES+1;i++){
             String path=String.format("src/students-092%d.txt",i);
             int numLines=getNumberLines(path);
@@ -452,7 +500,9 @@ public class Startup {
                 String[] values=s.split(",");
                 for (int i=0;i<values.length;i++){
                     if(i==0){ namesArray[lineNum]=values[i];}
-                    else{ respArr[lineNum][i-1]=((int)values[i].charAt(0)-ASCII_ZERO)==1;}
+                    else{
+                        respArr[lineNum][ i - 1 ]=((int)values[i].charAt(0)-ASCII_ZERO) == 1;
+                    }
                 }
                 lineNum++;
             }
